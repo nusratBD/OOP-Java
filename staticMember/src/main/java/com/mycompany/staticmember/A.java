7da6a53a9401a0a3e7a02a093a7eb4b50a414549
nusratBD <nusrat.hurain15@gmail.com>
2026-06-17 23:40:15 +0600

    Static Members

---
+++ staticMember/src/main/java/com/mycompany/staticmember/A.java
@@ -0,0 +1,24 @@
+package com.mycompany.staticmember;
+
+public class A {
+
+    static int a = 5;
+    int b = 5;
+    static int c;
+
+    static void display() {
+        System.out.println("a= " + (a + 10));
+        System.out.println("c= " + c);
+        //System.out.println("b= "+b);
+        /*Non-static variable b cannot be 
+        referenced from a static context.*/
+    }
+
+    static {
+        c = a * 3;
+    }
+
+//    public static void main(String args[]) {
+//        display();
+//    }
+}
---
+++ staticMember/src/main/java/com/mycompany/staticmember/StaticMember.java
@@ -0,0 +1,13 @@
+package com.mycompany.staticmember;
+public class StaticMember {
+
+    public static void main(String[] args) {
+//--------------Static Variable Access---------------
+        System.out.println(A.a);
+        //System.out.println(A.b); Not Possible
+        System.out.println(A.c);
+//--------------Static Method Access---------------
+        //A.display();
+        
+    }
+}
